e04e7ddd0b13dc6b9d49e7da4010f3296e5c5fc2
seangsengly01 <155617307+seangsengly01@users.noreply.github.com>
2024-07-30 15:43:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function toggleDarkMode() {
     document.body.classList.toggle('light-mode');
     const icon = document.getElementById('dark-mode-icon');
     if (document.body.classList.contains('light-mode')) {
-      icon.src = 'image/night-mode.png';
+      icon.src = 'night-mode.png';
       icon.style.transform = 'rotate(360deg)';
     } else {
-      icon.src = 'image/day-mode.png';
+      icon.src = 'day-mode.png';
       icon.style.transform = 'rotate(0deg)';
     }
   }
